10cfdac2ef1a559755b86f605930098ae4710874
Steven Kideckel <stevekideckel@gmail.com>
2024-02-19 17:33:04 -0500

    New android release, finally

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -111,8 +111,8 @@ android {
         applicationId = "com.sixbynine.transit.path"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 23
-        versionName = "2024.02.19.2"
+        versionCode = 24
+        versionName = "2024.02.19.3"
         resourceConfigurations += setOf("en", "es")
     }
     buildFeatures {

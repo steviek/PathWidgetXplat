b44081c4ece44cba1a7148b101ab5c9225d892e4
Steven Kideckel <stevekideckel@gmail.com>
2024-04-11 22:39:42 -0400

    Update alerts

--- api/src/commonTest/kotlin/com/sixbynine/transit/path/GithubAlertsTest.kt
+++ api/src/commonTest/kotlin/com/sixbynine/transit/path/GithubAlertsTest.kt
@@ -25,6 +25,7 @@ import kotlinx.datetime.Month.DECEMBER
 import kotlinx.datetime.Month.JANUARY
 import kotlinx.datetime.Month.JULY
 import kotlinx.datetime.Month.JUNE
+import kotlinx.datetime.atTime
 import kotlinx.serialization.encodeToString
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -35,57 +36,9 @@ class GithubAlertsTest {
     @Test
     fun `current alerts text`() {
         val alerts = GithubAlerts(
-            Alert(
-                stations = listOf(GroveStreet),
-                schedule = Schedule.repeatingWeekly(
-                    startDay = SATURDAY,
-                    startTime = LocalTime(6, 0),
-                    endDay = MONDAY,
-                    endTime = LocalTime(0, 0),
-                    from = LocalDate(2024, APRIL, 6),
-                    to = LocalDate(2024, JUNE, 30),
-                ),
-                displaySchedule = Schedule.repeatingDaily(
-                    days = listOf(SATURDAY, SUNDAY),
-                    start = LocalTime(0, 0),
-                    end = LocalTime(0, 0),
-                    from = LocalDate(2024, APRIL, 6),
-                    to = LocalDate(2024, JUNE, 30),
-                ),
-                trains = TrainFilter.headSigns("33rd", "World Trade"),
-                message = AlertText(
-                    en = "Due to construction, World Trade Center- and 33 St.-bound trains will not stop at Grove Street Station for most weekends between April 6 and June 30, 2024 from 6 AM Saturday - 11:59 PM Sunday, requiring customers to take detours for service to that station.",
-                    es = "Debido a construcción, los trenes con destino al World Trade Center y a 33 Street no pararán en la estación Grove Street durante la mayoría de los fines de semana entre el 6 de abril y el 30 de junio, desde las 6 a. m. del sábado hasta las 11:59 p. m. del domingo , lo que requerirá que los clientes tomen desvíos para el servicio a esa estación."
-                ),
-                url = AlertText(
-                    en = "https://www.panynj.gov/path/en/modernizing-path/grove-st-improvements.html"
-                )
-            ),
-            Alert(
-                stations = listOf(NinthStreet, TwentyThirdStreet),
-                schedule = Schedule.repeatingDaily(
-                    days = DayOfWeek.values().toList(),
-                    start = LocalTime(0, 0),
-                    end = LocalTime(5, 0),
-                    from = LocalDate(2024, JANUARY, 1),
-                    to = LocalDate(2025, DECEMBER, 31),
-                ),
-                displaySchedule = Schedule.repeatingDaily(
-                    days = DayOfWeek.values().toList(),
-                    start = LocalTime(17, 0),
-                    end = LocalTime(5, 0),
-                    from = LocalDate(2024, APRIL, 6),
-                    to = LocalDate(2024, JUNE, 30),
-                ),
-                trains = TrainFilter.all(),
-                message = AlertText(
-                    en = "9 St. & 23 St. stations are closed daily from 11:59 PM – 5 AM for maintenance-related activity. Please use Christopher St., 14 St., or 33 St. station.",
-                    es = "Las estaciones de 9 St. y 23 St. están cerradas diariamente de 11:59 p. m. a 5 a. m. para actividades relacionadas con el mantenimiento. Utilice las estaciones de Christopher St., 14 St. o 33 St."
-                ),
-                url = AlertText(
-                    en = "https://www.panynj.gov/path/en/schedules-maps.html"
-                )
-            )
+            GeneralGroveStAlert,
+            April12GroveStAlert,
+            GeneralOvernightCleaning,
         )
 
         val json = JsonFormat.encodeToString(alerts)
@@ -255,5 +208,80 @@ class GithubAlertsTest {
         assertTrue(alert.hidesTrain("09S", "33rd St"))
         assertTrue(alert.hidesTrain("23S", "33rd St"))
         assertTrue(alert.hidesTrain("23S", "Hoboken"))
+    }
+
+    private companion object {
+        val GeneralGroveStAlert = Alert(
+            stations = listOf(GroveStreet),
+            schedule = Schedule.repeatingWeekly(
+                startDay = SATURDAY,
+                startTime = LocalTime(6, 0),
+                endDay = MONDAY,
+                endTime = LocalTime(0, 0),
+                from = LocalDate(2024, APRIL, 17),
+                to = LocalDate(2024, JUNE, 30),
+            ),
+            displaySchedule = Schedule.repeatingDaily(
+                days = listOf(SATURDAY, SUNDAY),
+                start = LocalTime(0, 0),
+                end = LocalTime(0, 0),
+                from = LocalDate(2024, APRIL, 17),
+                to = LocalDate(2024, JUNE, 30),
+            ),
+            trains = TrainFilter.headSigns("33rd", "World Trade"),
+            message = AlertText(
+                en = "Due to construction, World Trade Center- and 33 St.-bound trains will not stop at Grove Street Station for most weekends between April 6 and June 30, 2024 from 6 AM Saturday - 11:59 PM Sunday, requiring customers to take detours for service to that station.",
+                es = "Debido a construcción, los trenes con destino al World Trade Center y a 33 Street no pararán en la estación Grove Street durante la mayoría de los fines de semana entre el 6 de abril y el 30 de junio, desde las 6 a. m. del sábado hasta las 11:59 p. m. del domingo , lo que requerirá que los clientes tomen desvíos para el servicio a esa estación."
+            ),
+            url = AlertText(
+                en = "https://www.panynj.gov/path/en/modernizing-path/grove-st-improvements.html"
+            )
+        )
+
+        val April12GroveStAlert = Alert(
+            stations = listOf(GroveStreet),
+            schedule = Schedule.once(
+                from = LocalDate(2024, APRIL, 13).atTime(6, 15),
+                to = LocalDate(2024, APRIL, 14).atTime(3, 0),
+            ),
+            displaySchedule = Schedule.once(
+                from = LocalDate(2024, APRIL, 12).atTime(18, 0),
+                to = LocalDate(2024, APRIL, 14).atTime(23, 59),
+            ),
+            trains = TrainFilter.headSigns("World Trade", "33rd"),
+            message = AlertText(
+                en = "NOTE: Starting at 3 AM on Sunday, April 14, all trains will stop at Grove Street Station to accommodate riders participating in the Jersey City Marathon. Due to construction, World Trade Center- and 33 St.-bound trains will not stop at Grove Street Station for most weekends between April 6 and June 30, 2024 from 6 AM Saturday - 11:59 PM Sunday, requiring customers to take detours for service to that station.",
+                es = "NOTA: A partir de las 3 a. m. del domingo 14 de abril, todos los trenes pararán en la estación Grove Street para acomodar a los pasajeros que participen en el maratón de la ciudad de Jersey. Debido a construcción, los trenes con destino al World Trade Center y a 33 Street no pararán en la estación Grove Street durante la mayoría de los fines de semana entre el 6 de abril y el 30 de junio, desde las 6 a. m. del sábado hasta las 11:59 p. m. del domingo , lo que requerirá que los clientes tomen desvíos para el servicio a esa estación."
+            ),
+            url = AlertText(
+                en = "https://www.panynj.gov/path/en/modernizing-path/grove-st-improvements/grove-street-bypass-schedule.html"
+            )
+        )
+
+        val GeneralOvernightCleaning = Alert(
+            stations = listOf(NinthStreet, TwentyThirdStreet),
+            schedule = Schedule.repeatingDaily(
+                days = DayOfWeek.values().toList(),
+                start = LocalTime(0, 0),
+                end = LocalTime(5, 0),
+                from = LocalDate(2024, JANUARY, 1),
+                to = LocalDate(2025, DECEMBER, 31),
+            ),
+            displaySchedule = Schedule.repeatingDaily(
+                days = DayOfWeek.values().toList(),
+                start = LocalTime(17, 0),
+                end = LocalTime(5, 0),
+                from = LocalDate(2024, APRIL, 6),
+                to = LocalDate(2024, JUNE, 30),
+            ),
+            trains = TrainFilter.all(),
+            message = AlertText(
+                en = "9 St. & 23 St. stations are closed daily from 11:59 PM – 5 AM for maintenance-related activity. Please use Christopher St., 14 St., or 33 St. station.",
+                es = "Las estaciones de 9 St. y 23 St. están cerradas diariamente de 11:59 p. m. a 5 a. m. para actividades relacionadas con el mantenimiento. Utilice las estaciones de Christopher St., 14 St. o 33 St."
+            ),
+            url = AlertText(
+                en = "https://www.panynj.gov/path/en/schedules-maps.html"
+            )
+        )
     }
 }

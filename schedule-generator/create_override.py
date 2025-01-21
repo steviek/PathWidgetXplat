d153436d0453e792ceb7665c5be605bd65efc20d
Austin Huang <im@austinhuang.me>
2025-01-21 16:59:34 -0500

    fix(ci): only get schedules after today
    there is no guarantee that the PATH page removes old holiday schedules on time; this was an issue during new year where the final December weekend schedule remained until January 1st

--- schedule-generator/create_override.py
+++ schedule-generator/create_override.py
@@ -12,9 +12,9 @@ def create_override(page: dict) -> dict:
             continue
         link = item['linkHref'].split(".html")[0]
         s, d = parse_schedule(page[":children"][link][":items"]["root"][":items"])
-        if s == None:
+        # if no schedule or date is before today
+        if s == None or d < datetime.now():
             continue
-
         # get only the closest date
         if date == None:
             data = s
